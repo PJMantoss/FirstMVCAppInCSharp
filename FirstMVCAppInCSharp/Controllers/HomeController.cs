dcc2c4ee5c42b4753546a058d6089f1fe68ce490
PJMantoss <joelptoss@gmail.com>
2022-12-22 16:32:55 +0100

    update HomeControllers

--- FirstMVCAppInCSharp/Controllers/HomeController.cs
+++ FirstMVCAppInCSharp/Controllers/HomeController.cs
@@ -17,6 +17,7 @@ namespace FirstMVCAppInCSharp.Controllers
         {
             Student student = new Student();
             student.Name = "David";
+            student.City = "Kaduna";
             return View();
         }
 
